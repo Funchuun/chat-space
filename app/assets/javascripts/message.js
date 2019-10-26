bff8f8a4ab2bc08b4d68f784b7f66e86d7fbdba7
Funchuun <kobf1w07@gmail.com>
2019-10-26 17:57:26 +0900

    update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -68,8 +68,7 @@ $(function() {
           alert('自動更新に失敗しました')
         })
       }
-      // setInterval(reloadMessages, 5000);
+      setInterval(reloadMessages, 5000);
     });
   });
-  setInterval(reloadMessages, 5000);
 });
