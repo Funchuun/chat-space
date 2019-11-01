e6835cf64bb722fcc4d36fdc3190cf729b760f1e
Funchuun <kobf1w07@gmail.com>
2019-11-01 12:22:43 +0900

    Update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,56 +1,78 @@
-$(function() {
-  $(document).on('turbolinks:load', function(){
-    function buildHTML(message){
-      var image = message.image.url ?  `<img src="${message.image.url}">` : '';
-      var html = `<div class="message" data-id="${message.id}">
-                      <div class="upper-message">
-                        <div class="upper-message__user-name">
-                          ${message.user_name}
-                        </div>
-                        <div class="upper-message__date">
-                          ${message.created_at}
-                        </div>
+$(document).on('turbolinks:load', function(){
+  function buildHTML(message){
+    var image = message.image.url ?  `<img src="${message.image.url}">` : '';
+    var html = `<div class="message" data-id="${message.id}">
+                    <div class="upper-message">
+                      <div class="upper-message__user-name">
+                        ${message.user_name}
                       </div>
-                      <div class="lower-message">
-                        <p class="lower-message__content">
-                          ${message.content}
-                        </p>
+                      <div class="upper-message__date">
+                        ${message.created_at}
                       </div>
-                      ${image}
-                    </div>`;
-        return html;
-    }
+                    </div>
+                    <div class="lower-message">
+                      <p class="lower-message__content">
+                        ${message.content}
+                      </p>
+                    </div>
+                    ${image}
+                  </div>`;
+      return html;
+  }
 
-    $('#new_message').on('submit', function(e){
-      e.preventDefault();
-      var formData = new FormData(this);
-      var url = $(this).attr("action")
-      $.ajax({
-        url: url,
-        type: "POST",
-        data: formData,
-        dataType: 'json',
-        processData: false,
-        contentType: false
-      })
-      .done(function(data){
-        var html = buildHTML(data);
-        $('.messages').append(html);
-        $('.new_message')[0].reset();
-        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
-        $('.form__submit').prop('disabled', false);
-      })
-      .fail(function(){
-        alert('no message');
-      })
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr("action")
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
     })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.new_message')[0].reset();
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
+      $('.form__submit').prop('disabled', false);
+    })
+    .fail(function(){
+      alert('no message');
+    })
+  })
 
-    $(function() {
-      var reloadMessages = function() {
-        last_message_id = $('.message').last().data('id');
-        var url = 'api/messages'
+  $(function() {
+    var reloadMessages = function() {
+      last_message_id = $('.message').last().data('id');
+      group_id = $('.left-header__title').data('group_id');
+      var url = location.href;
+      if (url.match(/\/groups\/\d+\/messages/)) {
+        function buildHTML(message){
+          var image = message.image.url ?  `<img src="${message.image.url}">` : '';
+          var html = `<div class="message" data-id="${message.id}">
+                          <div class="upper-message">
+                            <div class="upper-message__user-name">
+                              ${message.user_name}
+                            </div>
+                            <div class="upper-message__date">
+                              ${message.created_at}
+                            </div>
+                          </div>
+                          <div class="lower-message">
+                            <p class="lower-message__content">
+                              ${message.content}
+                            </p>
+                          </div>
+                          ${image}
+                        </div>`;
+            return html;
+        }
+        var api_url = 'api/messages'
         $.ajax({
-          url: url,
+          url: api_url,
           type: 'GET',
           dataType: 'json',
           data: {id: last_message_id}
@@ -68,7 +90,7 @@ $(function() {
           alert('自動更新に失敗しました')
         })
       }
-      setInterval(reloadMessages, 5000);
-    });
+    }
+    setInterval(reloadMessages, 5000);
   });
 });
